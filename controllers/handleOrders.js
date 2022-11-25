23bf4f588ad250edd1f121213dd24da16c3390f6
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-25 20:32:39 +0500

    set basic express boilerplate

---
+++ controllers/handleOrders.js
@@ -0,0 +1,5 @@
+export const handleOrders = (req, res) => {
+    res.json({
+        success: true
+    })
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+import { handleOrders } from './controllers/handleOrders.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+        
+app.get('/', handleOrders); 
+
+app.listen(process.env.PORT || 8800, () => {
+    console.log('server is working properly');
+});
