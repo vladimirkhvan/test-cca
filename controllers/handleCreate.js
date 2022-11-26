8aaad14d0905599064b488aa18e06107d5c91c8b
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 17:57:46 +0500

    posting record

--- controllers/handleCreate.js
+++ controllers/handleCreate.js
@@ -2,17 +2,18 @@ import axios from 'axios';
 import BP from 'bp-api';
 
 export const handleCreate = async (req, res) => {
-
     const bp = new BP('test_vladimir.bpium.ru', 'vladimirkhvan23@gmail.com', '612554097');
 
     const record = await bp.getRecordById(req.body.payload.catalogId, req.body.payload.recordId);
 
     console.log(record);
 
-    // const response = await bp.postRecord(req.body.payload.catalogId, {
-    //     3: req.body.payload.catalogId,
-    //     4: req.body.payload
-    // })
+    const response = await bp.postRecord(req.body.payload.catalogId, {
+        3: req.body.payload.catalogId,
+        4: req.body.payload.values['3'],
+    });
+
+    console.log(response);
 
     // const response = await bp.patchRecord(req.body.payload.catalogId, req.body.payload.recordId, {
     //     2: [req.body.payload.values['2'][0]],
