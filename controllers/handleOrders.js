b20a40bde6ca74877e1f250bdcfbac8bfbeeeb28
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 16:43:47 +0500

    parsing value | patching record

--- controllers/handleOrders.js
+++ controllers/handleOrders.js
@@ -1,8 +1,27 @@
-export const handleOrders = (req, res) => {
+import axios from 'axios';
+import BP from 'bp-api';
 
-    console.log(req.body);
+export const handleOrders = async (req, res) => {
+    const { value } = await axios.get('https://test.bpium.ru/api/webrequest/request');
+
+    console.log(value);
+    console.log(req.body.payload);
+
+    const bp = new BP(
+        'https://test_vladimir.bpium.ru/',
+        'vladimirkhvan23@gmail.com',
+        '612554097',
+        protocol('https'),
+        timeout(30000),
+    );
+
+    await bp.patchRecord(req.body.payload.catalogId, req.body.payload.recordId, {
+        3: value,
+    });
+
+    console.log(value);
 
     res.json({
-        success: true
-    })
+        success: true,
+    });
 };
