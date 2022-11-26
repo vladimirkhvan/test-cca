e2d43ffa8c99a7ecc218523910ad1238bfda11ff
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 18:21:32 +0500

    formed object

--- controllers/handleCreate.js
+++ controllers/handleCreate.js
@@ -5,19 +5,29 @@ export const handleCreate = async (req, res) => {
     const bp = new BP('test_vladimir.bpium.ru', 'vladimirkhvan23@gmail.com', '612554097');
 
     const record = await bp.getRecordById(req.body.payload.catalogId, req.body.payload.recordId);
-    
+
     const repLog = await bp.getRecordById('12', 1);
 
     console.log('record is ', record);
 
     console.log('rep log is ', repLog);
 
-    console.log('rep log object is ', repLog.values['3'][0])
+    console.log('rep log object is ', repLog.values['3'][0]);
 
     const response = await bp.postRecord('12', {
-        3: record,  
+        3: {
+            sectionId: '3',
+            catalogId: '12',
+            catalogTitle: 'Склад',
+            catalogIcon: 'content-11',
+            recordId: record.id,
+            recordTitle: record.title,
+            isRemoved: false,
+        },
         4: record.values['3'],
-    }); 
+    });
+
+    console.log('response is ', response);
 
     res.json({
         success: true,
