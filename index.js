a8b35297a94cf342bee9a8bb6990dc7083971bb6
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 17:53:23 +0500

    add new route

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 
 import { handleOrders } from './controllers/handleOrders.js';
+import { handleCreate } from './controllers/handleCreate.js';
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
                 
 app.post('/', handleOrders); 
+app.post('/oncreate', handleCreate); 
 
 app.listen(process.env.PORT || 8800, () => {
     console.log('server is working properly');
