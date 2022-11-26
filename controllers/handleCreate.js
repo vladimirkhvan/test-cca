ebc72d06c6a68daa8c43d2388375c6b0bce89fcf
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 18:10:25 +0500

    watching object

--- controllers/handleCreate.js
+++ controllers/handleCreate.js
@@ -5,25 +5,17 @@ export const handleCreate = async (req, res) => {
     const bp = new BP('test_vladimir.bpium.ru', 'vladimirkhvan23@gmail.com', '612554097');
 
     const record = await bp.getRecordById(req.body.payload.catalogId, req.body.payload.recordId);
-
-    console.log(record);
-    console.log(record.values['3']);
-
+    
     const repLog = await bp.getRecordById('12', 1);
 
     console.log('rep log is ', repLog);
 
+    console.log('rep log object is ', repLog.values['3'][0])
+
     const response = await bp.postRecord('12', {
-        3: record.values['3'],
+        3: [record.values['3']],
         4: record.values['3'],
     });
-
-    console.log(response);
-
-    // const response = await bp.patchRecord(req.body.payload.catalogId, req.body.payload.recordId, {
-    //     2: [req.body.payload.values['2'][0]],
-    //     3: data.value,
-    // });
 
     res.json({
         success: true,
