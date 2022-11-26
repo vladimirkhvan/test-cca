09f181a0bbb6508007c66c1f8b479dac3acb3fb2
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 16:50:05 +0500

    init BP api

--- controllers/handleOrders.js
+++ controllers/handleOrders.js
@@ -2,18 +2,18 @@ import axios from 'axios';
 import BP from 'bp-api';
 
 export const handleOrders = async (req, res) => {
-    const value = await axios.get('https://test.bpium.ru/api/webrequest/request');
+    const {data} = await axios.get('https://test.bpium.ru/api/webrequest/request');
 
-    console.log(value);
+    console.log(data.value);
     console.log(req.body.payload);
 
-    // const bp = new BP(
-    //     'https://test_vladimir.bpium.ru/',
-    //     'vladimirkhvan23@gmail.com',
-    //     '612554097',
-    //     protocol(https),
-    //     timeout(30000),
-    // );
+    const bp = new BP(
+        'https://test_vladimir.bpium.ru/',
+        'vladimirkhvan23@gmail.com',
+        '612554097',
+        protocol(https),
+        timeout(30000),
+    );
 
     // await bp.patchRecord(req.body.payload.catalogId, req.body.payload.recordId, {
     //     3: value,
