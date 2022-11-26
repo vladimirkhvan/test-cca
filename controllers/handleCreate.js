878c26ae29f120cf7927181502e48820e3681500
vladimirkhvan <vladimirkhvan23@gmail.com>
2022-11-26 18:26:40 +0500

    formed object

--- controllers/handleCreate.js
+++ controllers/handleCreate.js
@@ -15,7 +15,7 @@ export const handleCreate = async (req, res) => {
     console.log('rep log object is ', repLog.values['3'][0]);
 
     const response = await bp.postRecord('12', {
-        3: {
+        3: [{
             sectionId: '3',
             catalogId: '12',
             catalogTitle: 'Склад',
@@ -23,7 +23,7 @@ export const handleCreate = async (req, res) => {
             recordId: record.id,
             recordTitle: record.title,
             isRemoved: false,
-        },
+        }],
         4: record.values['3'],
     });
 
